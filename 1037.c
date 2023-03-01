59e05a0b8fae052ae420d08b703670db8bba086f
Roudra Mondal <122085571+Roudra25@users.noreply.github.com>
2023-03-01 11:46:47 +0600

    Add files via upload

---
+++ 1037.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+    float n;
+    scanf("%f",&n);
+    if(n>=0 && n<=25.00)
+        printf("Intervalo [0,25]\n");
+    else if(n>=25.01&&n<=50.00)
+        printf("Intervalo (25,50]\n");
+
+
+    else if(n>=50.0001&&n<=75.00)
+        printf("Intervalo (50,75]\n");
+
+     else if(n>=75.0001&&n<=100.00)
+        printf("Intervalo (75,100]\n");
+    else
+        printf("Fora de intervalo\n");
+
+    return 0;
+
+}
